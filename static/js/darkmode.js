607695b586d8fb4fd2db6010afdc84f3e0dfd494
arpit456jain <111arpit1@gmail.com>
2021-03-26 22:52:29 +0530

    "fix bug in register and remove comments in DM.js"

--- static/js/darkmode.js
+++ static/js/darkmode.js
@@ -1,7 +1,7 @@
 var element = document.body;
 // var togglebutton = document.getElementById("toggle");
 const currentTheme = localStorage.getItem("theme");
-console.log("helo", currentTheme);
+// console.log("helo", currentTheme);
 // If the current theme in localStorage is "dark"...
 if (currentTheme == "dark") {
   console.log("Its dark");
@@ -10,23 +10,23 @@ if (currentTheme == "dark") {
   theme = "dark";
   localStorage.setItem("theme", theme);
 } else {
-  console.log("Its Light");
+  // console.log("Its Light");
   // togglebutton.innerHTML = "Dark Mode";
   theme = "light";
   localStorage.setItem("theme", theme);
 }
 
 function myfun() {
-  console.log("myFunction");
+  // console.log("myFunction");
   const currentTheme = localStorage.getItem("theme");
   if (currentTheme == "dark") {
-    console.log("prev itss dark now it is light");
+    // console.log("prev itss dark now it is light");
     element.classList.toggle("dark-mode");
     theme = "light";
     localStorage.setItem("theme", theme);
     // togglebutton.innerHTML = "Dark Mode";
   } else {
-    console.log("prev is light now its dark");
+    // console.log("prev is light now its dark");
     element.classList.toggle("dark-mode");
     theme = "dark";
     localStorage.setItem("theme", theme);

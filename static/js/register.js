e5714a7d270db95e5f7769d0a1970a33070cabc9
arpit456jain <111arpit1@gmail.com>
2021-03-28 13:19:50 +0530

    added confirm password validation

--- static/js/register.js
+++ static/js/register.js
@@ -1,21 +1,40 @@
 var myInput = document.getElementById("psw");
+var myinput2 = document.getElementById("psw2");
+document.getElementById('registerBtn').disabled = true;
   var letter = document.getElementById("letter");
   var capital = document.getElementById("capital");
   var number = document.getElementById("number");
   var length = document.getElementById("length");
-  
+  var passwordmatch = document.getElementById("passwordmatch");
+  console.log(passwordmatch)
   // When the user clicks on the password field, show the message box
   myInput.onfocus = function() {
-    document.getElementById("message").style.display = "block";
+    document.getElementById("message").style.visibility = "visible";
   }
   
   // When the user clicks outside of the password field, hide the message box
   myInput.onblur = function() {
-    document.getElementById("message").style.display = "none";
+    document.getElementById("message").style.visibility = "hidden";
   }
+
+  
+
+  // for confirm password
+  
+ // When the user clicks on the password field, show the message box
+ myinput2.onfocus = function() {
+  document.getElementById("message2").style.visibility = "visible";
+}
+
+// When the user clicks outside of the password field, hide the message box
+myinput2.onblur = function() {
+  document.getElementById("message2").style.visibility = "hidden";
+}
+
   
   // When the user starts to type something inside the password field
   myInput.onkeyup = function() {
+    document.getElementById('registerBtn').disabled = true;
     // Validate lowercase letters
     var lowerCaseLetters = /[a-z]/g;
     if(myInput.value.match(lowerCaseLetters)) {  
@@ -54,4 +73,21 @@ var myInput = document.getElementById("psw");
       length.classList.remove("valid");
       length.classList.add("invalid");
     }
+  }
+
+  myinput2.onkeyup = function() {
+    console.log(myInput.value,myinput2.value);
+    // validating confirm password
+    if (myInput.value == myinput2.value)
+    {
+        passwordmatch.classList.remove("invalid");
+        passwordmatch.classList.add("valid");
+        document.getElementById('registerBtn').disabled = false;
+    }
+    else
+    {
+        passwordmatch.classList.remove("valid");
+        passwordmatch.classList.add("invalid");
+    }
+  
   }

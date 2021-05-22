2262bc690c18b41b0f413c23040cb8566affa12e
Subhangi Choudhary <subhangichoudhary6@gmail.com>
2021-05-22 12:09:19 +0530

    added dark mode UI

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Provider } from "react-redux";
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Provider>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function

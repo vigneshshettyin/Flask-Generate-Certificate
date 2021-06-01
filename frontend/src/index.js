ebc58c11160d8f0d8ead6b6d32c5f6fe7a7d7696
Vignesh Shetty <77713888+vigneshshettyin@users.noreply.github.com>
2021-06-01 01:10:18 -0700

    Frontend React Removed

--- frontend/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Main from "./Main";
-import "./index.css";
- 
-ReactDOM.render(
-  <Main/>, 
-  document.getElementById("root")
-); 
